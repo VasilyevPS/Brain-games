f65c131615f491c8611c3a62189c1a3e5420e368
Pavel Vasilyev <pavel.vas@mail.ru>
2023-02-19 17:45:35 +0300

    Add game choice to App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args) {
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int gameOption = scanner.nextInt();
+
+        if (gameOption == 1) {
+            Cli.greeting();
+        }
+
+        scanner.close();
     }
 }
