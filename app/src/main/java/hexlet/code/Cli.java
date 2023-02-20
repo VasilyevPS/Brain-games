f8b6b8575cb2a49c4b6113d2a2a8c6cb00ce6a36
Pavel Vasilyev <pavel.vas@mail.ru>
2023-02-20 19:42:25 +0300

    Add variable userName return for method

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,14 +3,14 @@ package hexlet.code;
 import java.util.Scanner;
 
 class Cli {
-    static void greeting() {
+    static String greeting() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
+        return userName;
 
-        scanner.close();
     }
 }
