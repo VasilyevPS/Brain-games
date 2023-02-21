9280d29aa93318e3e83b38bac16acd8cb368c5b5
Pavel Vasilyev <pavel.vas@mail.ru>
2023-02-21 21:48:35 +0300

    Replace magic numbers with constants

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,19 +1,19 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
-import java.util.Random;
 
 public class Calc {
+    static final int MAX_NUMBER = 20;
     public static void calcGame() {
         var questionMessage = "What is the result of the expression?";
         String[] questions = new String[Engine.NUMBER_OF_ROUNDS];
         String[] answers = new String[Engine.NUMBER_OF_ROUNDS];
 
         for (var n = 0; n < Engine.NUMBER_OF_ROUNDS; n++) {
-            var firstNumber = (int) (Math.random() * 20);
-            var secondNumber = (int) (Math.random() * 20);
-            Random signChoice = new Random();
-            var sign = "+-*".charAt(signChoice.nextInt(3));
+            var firstNumber = (int) (Math.random() * MAX_NUMBER);
+            var secondNumber = (int) (Math.random() * MAX_NUMBER);
+            var signOption = "+-*";
+            var sign = signOption.charAt((int) (Math.random() * signOption.length()));
             questions[n] = firstNumber + " " + sign + " " + secondNumber;
             if (sign == '+') {
                 answers[n] = firstNumber + secondNumber + "";
--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -3,13 +3,14 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Even {
+    static final int MAX_NUMBER = 100;
     public static void evenGame() {
         var questionMessage = "Answer 'yes' if the number is even, otherwise answer 'no'.";
         String[] questions = new String[Engine.NUMBER_OF_ROUNDS];
         String[] answers = new String[Engine.NUMBER_OF_ROUNDS];
 
         for (var n = 0; n < Engine.NUMBER_OF_ROUNDS; n++) {
-            int number = (int) (Math.random() * 100);
+            int number = (int) (Math.random() * MAX_NUMBER);
             questions[n] = number + "";
             if (number % 2 == 0) {
                 answers[n] = "yes";
