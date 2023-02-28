cc1a5ba8207f5943e192d973223c119662fdb3c4
Pavel Vasilyev <pavel.vas@mail.ru>
2023-02-28 18:58:52 +0300

    Separate auxiliary methods from gcdGame

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -9,14 +9,19 @@ public class Gcd {
         String[][] roundData = new String[Engine.NUMBER_OF_ROUNDS][2];
 
         for (var round : roundData) {
-            var firstNumber = (int) (Math.random() * MAX_NUMBER);
-            var secondNumber = (int) (Math.random() * MAX_NUMBER);
-            round[0] = firstNumber + " " + secondNumber;
-            round[1] = gcd(firstNumber, secondNumber) + "";
+            generateGcdData(round);
         }
+
         Engine.gameProcess(questionMessage, roundData);
     }
 
+    public static void generateGcdData(String[] round) {
+        var firstNumber = (int) (Math.random() * MAX_NUMBER);
+        var secondNumber = (int) (Math.random() * MAX_NUMBER);
+        round[0] = firstNumber + " " + secondNumber;
+        round[1] = gcd(firstNumber, secondNumber) + "";
+    }
+    
     public static int gcd(int a, int b) {
         if (a % b == 0) {
             return Math.abs(b);
