0a90e3c8ecb70f27b6c44633872c32dcbd9e516c
Pavel Vasilyev <pavel.vas@mail.ru>
2023-02-28 20:24:31 +0300

    Refactor Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,26 +5,22 @@ import java.util.Scanner;
 public class Engine {
     public static final int NUMBER_OF_ROUNDS = 3;
 
-    public static void gameProcess(String questionMessage, String[][] data) {
+    public static void gameProcess(String questionMessage, String[][] roundData) {
         String userName = Cli.greeting();
         System.out.println(questionMessage);
         Scanner scanner = new Scanner(System.in);
-        int count = 0;
-        while (count < NUMBER_OF_ROUNDS) {
-            System.out.println("Question: " + data[count][0]);
+
+        for (var round : roundData) {
+            System.out.println("Question: " + round[0]);
             System.out.print("Your answer: ");
             String answer = scanner.next();
-            if (answer.equals(data[count][1])) {
-                System.out.println("Correct!");
-                count++;
-            } else {
-                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + data[count][1] + "'.");
+            if (!answer.equals(round[1])) {
+                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + round[1] + "'.");
                 System.out.println("Let's try again, " + userName + "!");
-                break;
+                return;
             }
+            System.out.println("Correct!");
         }
-        if (count == NUMBER_OF_ROUNDS) {
-            System.out.println("Congratulations, " + userName + "!");
-        }
+        System.out.println("Congratulations, " + userName + "!");
     }
 }
