ba6c71d8dd64df60b21bafe22676bbae64d85a12
Pavel Vasilyev <pavel.vas@mail.ru>
2023-02-28 18:54:01 +0300

    Separate auxiliary methods from calcGame

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -9,19 +9,28 @@ public class Calc {
         String[][] roundData = new String[Engine.NUMBER_OF_ROUNDS][2];
 
         for (var round : roundData) {
-            var firstNumber = (int) (Math.random() * MAX_NUMBER);
-            var secondNumber = (int) (Math.random() * MAX_NUMBER);
-            var signOption = "+-*";
-            var sign = signOption.charAt((int) (Math.random() * signOption.length()));
-            round[0] = firstNumber + " " + sign + " " + secondNumber;
-            if (sign == '+') {
-                round[1] = firstNumber + secondNumber + "";
-            } else if (sign == '-') {
-                round[1] = firstNumber - secondNumber + "";
-            } else {
-                round[1] = firstNumber * secondNumber + "";
-            }
+            generateCalcData(round);
         }
+
         Engine.gameProcess(questionMessage, roundData);
+    }
+
+    public static void generateCalcData(String[] round) {
+        var firstNumber = (int) (Math.random() * MAX_NUMBER);
+        var secondNumber = (int) (Math.random() * MAX_NUMBER);
+        var signOption = "+-*";
+        var sign = signOption.charAt((int) (Math.random() * signOption.length()));
+        round[0] = firstNumber + " " + sign + " " + secondNumber;
+        round[1] = calculateAnswer(firstNumber, secondNumber, sign);
+    }
+
+    public static String calculateAnswer(int firstNumber, int secondNumber, char sign) {
+        if (sign == '+') {
+            return firstNumber + secondNumber + "";
+        } else if (sign == '-') {
+            return firstNumber - secondNumber + "";
+        } else {
+            return firstNumber * secondNumber + "";
+        }
     }
 }
